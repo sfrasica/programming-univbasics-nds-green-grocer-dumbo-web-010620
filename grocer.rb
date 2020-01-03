f308ad660c028e7b4b941dc35fc25ffbd9c33fa6
Steven Frasica <sfrasica1@gmail.com>
2020-01-03 17:02:39 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,5 +1,13 @@
+CLEARANCE_ITEM_DISCOUNT_RATE = 0.20
+BIG_PURCHASE_DISCOUNT_RATE = 0.10
+
 def find_item_by_name_in_collection(name, collection)
-  # Implement me first!
+  i = 0
+  while i < collection.length do
+    return collection[i] if name === collection[i][:item]
+    i += 1
+  end
+  nil# Implement me first!
   #
   # Consult README for inputs and outputs
 end
